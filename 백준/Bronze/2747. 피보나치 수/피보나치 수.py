84f4ad2fdeb8323add31b01e41fd9e78153cbf0b
Park Jeong-Eun <109335097+je0ng3@users.noreply.github.com>
2024-01-06 21:17:44 +0900

    [Bronze II] Title: 피보나치 수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.py
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -1,10 +1,8 @@
-def fib(n):
-    a,b = 1, 1
-    if n==1 or n==2:
-        return 1
-    for i in range(1,n):
-        a,b = b, a+b
-    return a
-
-n = int(input())
-print(fib(n))
+n= int(input())
+B=[1,1]
+if n==1 or n==2:
+    print(1)
+else:
+    for i in range(2,n):
+        B.append(B[i-1]+B[i-2])
+    print(B[n-1])
