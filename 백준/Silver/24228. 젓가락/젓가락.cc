2b7c58b53899baa9fe09d86026937b381651643e
Park Jeong-Eun <109335097+je0ng3@users.noreply.github.com>
2024-02-26 23:13:57 +0900

    [Silver V] Title: 젓가락, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/24228. 젓가락/젓가락.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  long long n, r; cin>>n>>r;
+  cout<<n+r*2-1<<'\n';
+
+}
