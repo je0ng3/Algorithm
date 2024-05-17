ec4167d84f998e544d97c6c842cd42e668ff1105
Park Jeong-Eun <109335097+je0ng3@users.noreply.github.com>
2024-05-17 13:43:22 +0900

    [level 1] Title: 최소직사각형, Time: 3.67 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,11 @@
+def solution(sizes):
+    w, h = 0, 0
+    for size in sizes:
+        if size[0]>size[1]:
+            w = max(w,size[0])
+            h = max(h,size[1])
+        else:
+            w = max(w,size[1])
+            h = max(h,size[0])
+    answer = w*h
+    return answer
