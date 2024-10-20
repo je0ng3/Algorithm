eefdcf1c3cafe663e3abcbc598fffb00f31f97f7
Park Jeong-Eun <109335097+je0ng3@users.noreply.github.com>
2024-10-20 21:54:24 +0900

    [level 0] Title: 리스트 자르기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.py
@@ -0,0 +1,11 @@
+def solution(n, slicer, num_list):
+    answer = []
+    if n==1:
+        return num_list[:slicer[1]+1]
+    elif n==2:
+         return num_list[slicer[0]:]
+    elif n==3:
+        return num_list[slicer[0]:slicer[1]+1]
+    for i in range(slicer[0], slicer[1]+1, slicer[2]):
+        answer.append(num_list[i])
+    return answer
