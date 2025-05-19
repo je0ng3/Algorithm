ec18da3366ee593f81583842d7b354ebe70c21ca
Park Jeong-Eun <109335097+je0ng3@users.noreply.github.com>
2025-05-19 14:26:24 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,14 +1,13 @@
 def solution(n, lost, reserve):
-    answer = n
-    s1 = set(lost)
-    s2 = set(reserve)
-    s3 = s1-s2 # 체육복 없는 학생
-    s4 = s2-s1 # 여분 체육복 있는 학생
-    for i in sorted(s3):
-        if i-1 in s4:
-            s4.remove(i-1)
-        elif i+1 in s4:
-            s4.remove(i+1)
-        else:
-            answer-=1
-    return answer
+    cant = 0
+    a = list(set(reserve)-set(lost))
+    b = list(set(lost)-set(reserve))
+    for i in range(1, n+1):
+        if i in b:
+            if i-1 in a:
+                continue
+            elif i+1 in a:
+                a.remove(i+1)
+            else:
+                cant+=1
+    return n-cant
