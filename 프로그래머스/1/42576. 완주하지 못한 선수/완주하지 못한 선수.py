a98ee623932e98a57a84e7ae7c394a809949349e
Park Jeong-Eun <109335097+je0ng3@users.noreply.github.com>
2025-10-03 16:28:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 73.24 ms, Memory: 25.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,8 +1,7 @@
 def solution(participant, completion):
-    answer = ''
     participant.sort()
     completion.sort()
-    for i in range(len(completion)):
-        if participant[i] != completion[i]:
-            return participant[i]
+    for p, c in zip(participant, completion):
+        if p!=c:
+            return p
     return participant[-1]
