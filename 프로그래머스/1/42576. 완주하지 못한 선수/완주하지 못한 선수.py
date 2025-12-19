3292b2f2d86da59134ae88c04a0f776fea6e3289
Park Jeong-Eun <109335097+je0ng3@users.noreply.github.com>
2025-12-19 15:48:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 54.56 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,7 +1,11 @@
 def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-    for p, c in zip(participant, completion):
-        if p!=c:
-            return p
-    return participant[-1]
+    answer = ''
+    dic = {}
+    temp = 0
+    for p in participant:
+        dic[hash(p)] = p
+        temp += int(hash(p))
+    for c in completion:
+        temp -= hash(c)
+    answer = dic[temp]
+    return answer
