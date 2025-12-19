0bf7fc128198799ddfe44faa121a00c53ae08e58
Park Jeong-Eun <109335097+je0ng3@users.noreply.github.com>
2025-12-19 15:52:24 +0900

    [level 1] Title: 폰켓몬, Time: 0.99 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,6 +1,5 @@
 def solution(nums):
-    types = len(set(nums))
-    allowed = len(nums)/2
-    if types>allowed:
-        return allowed
-    return types
+    answer = 0
+    set_num = set(nums)
+    answer = min(len(set_num), len(nums)//2)
+    return answer
