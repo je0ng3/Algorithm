fdfa66595ad17dfa5128d47f25ab92d03fec34c4
Park Jeong-Eun <109335097+je0ng3@users.noreply.github.com>
2026-07-12 15:35:41 +0900

    [level 2] Title: 의상, Time: 0.07 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,12 +1,8 @@
 def solution(clothes):
     answer = 1
-    dic = {}
-    for c, t in clothes:
-        h = hash(t)
-        if h not in dic:
-            dic[h] = 2  # t 타입의 옷이 없는 경우 + 있는 경우 = 2
-        else:
-            dic[h] += 1
-    for v in dic.values():
+    kinds = {}
+    for name, kind in clothes:
+        kinds[kind] = kinds.get(kind, 1) + 1 # 초기 1인 이유: 안입을수도 있으니까
+    for v in kinds.values():
         answer *= v
-    return answer -1 # 아무것도 착용하지 않는 경우 1
+    return answer-1  # 모든 종류를 다 착용하지 않는 경우 제거
