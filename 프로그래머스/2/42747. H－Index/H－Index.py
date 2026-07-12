7e96307700a41846d5404ca262e187268ce2169e
Park Jeong-Eun <109335097+je0ng3@users.noreply.github.com>
2026-07-12 21:14:28 +0900

    [level 2] Title: H-Index, Time: 0.09 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,16 +1,11 @@
 def solution(citations):
-    h = -1
-    citations = sorted(citations)
     n = len(citations)
-    for i in range(citations[-1]+1):
-        idx = find_idx(citations, i)
-        if idx==-1 or n-idx<i:
-            break
-        h = max(h, i)
-    return h
+    sorted_c = sorted(citations)
+    for i, citation in enumerate(sorted_c):
+        h = n-i
+        if citation>=h:
+            return h
+    return 0
 
-def find_idx(citations, i):
-    for idx, c in enumerate(citations):
-        if i<=c:
-            return idx
-    return -1
+# h번 이상 인용된 논문이 h편 이상인 h의 최댓값
+
