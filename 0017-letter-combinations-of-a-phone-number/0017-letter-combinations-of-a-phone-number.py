743f071656ea6843711b6ae332aad0d558c8b89c
Park Jeong-Eun <109335097+je0ng3@users.noreply.github.com>
2026-07-20 12:44:26 +0900

    Time: 0 ms (100%), Space: 19.3 MB (77.28%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,17 +1,21 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        dic = {'2':"abc", '3':"def",
-                '4':"ghi", '5':"jkl", '6':"mno",
-                '7':"pqrs", '8':"tuv", '9':"wxyz"}
-        
+        dic = {
+            '2':'abc', '3':'def','4':'ghi','5':'jkl',
+            '6':'mno','7':'pqrs', '8':'tuv', '9':'wxyz'
+        }
+
         answer = []
-        def dfs(idx, path):
-            if len(path)==len(digits):
-                answer.append(path)
+        def dfs(idx, cur):
+            if len(cur)==len(digits):
+                answer.append(cur)
                 return
             for i in range(idx, len(digits)):
-                for j in dic[digits[i]]:
-                    dfs(i+1, path+j)
-        dfs(0, '')
+                for c in dic[digits[i]]:
+                    cur+=c
+                    dfs(i+1, cur)
+                    cur=cur[:-1]
 
+
+        dfs(0, '')
         return answer
