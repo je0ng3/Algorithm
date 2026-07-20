86c98a71e1780ef2fd903894f4384154e9ad82f4
Park Jeong-Eun <109335097+je0ng3@users.noreply.github.com>
2026-07-20 12:31:17 +0900

    Time: 231 ms (89.23%), Space: 21.6 MB (71.91%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,23 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
 
-        def dfs(self, grid, r, c):
-            if not (0<=r<len(grid) and 0<=c<len(grid[0])) \
-                or grid[r][c]!='1':
-                return
+        def dfs(grid, r, c):
             grid[r][c] = '0'
-            dfs(self, grid, r+1, c)
-            dfs(self, grid, r-1, c)
-            dfs(self, grid, r, c+1)
-            dfs(self, grid, r, c-1)
-        
-        if not grid:
-            return 0
-
+            dr = [0,0,1,-1]
+            dc = [1,-1,0,0]
+            for i in range(4):
+                nr, nc = r+dr[i], c+dc[i]
+                if 0<=nr<m and 0<=nc<n and grid[nr][nc]=='1':
+                    dfs(grid, nr, nc)
+            
         count = 0
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                if grid[r][c]=='1':
-                    dfs(self, grid, r, c)
-                    count +=1
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == '1':
+                    dfs(grid, r, c)
+                    count += 1
+        
         return count
