f26f696c212cc21047d83ca4a6e73a070f97f04e
Park Jeong-Eun <109335097+je0ng3@users.noreply.github.com>
2026-07-23 11:34:51 +0900

    Time: 0 ms (100%), Space: 19.5 MB (10.36%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,28 +1,33 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if not nums:
+        def binary_search(lst, t):
+            l,r=0,len(lst)
+            while l<=r:
+                mid = (l+r)//2
+                if lst[mid]<t:
+                    l=mid+1
+                elif lst[mid]>t:
+                    r=mid-1
+                else:
+                    return mid
             return -1
-        left, right = 0, len(nums)-1
-        while left<right:
-            mid = (left+right)//2
-            if nums[mid]>nums[right]:
-                left = mid + 1
-            else:
-                right = mid
-        pivot = left
 
-        if target>=nums[0]: 
-            left, right = 0, pivot-1
-            if right==-1:
-                right = len(nums)-1
+        if not nums or target not in set(nums):
+            return -1
+        answer = 0
+        # 정렬된 두 리스트로 분리
+        left = right = 0
+        while right<len(nums) and nums[left]<=nums[right]:
+            right+=1
+        # target이 속한 리스트 찾기
+        # 1. left~right-1
+        # 2. right~len(nums)-1
+        if nums[left]<=target<=nums[right-1]:
+            idx = binary_search(nums[left:right], target)
+            return idx
         else:
-            left, right = pivot, len(nums)-1
-        while left<=right:
-            mid = (left+right)//2
-            if nums[mid]>target:
-                right = mid-1
-            elif nums[mid]<target:
-                left = mid+1
+            idx = binary_search(nums[right:len(nums)], target)
+            if idx==-1:
+                return idx
             else:
-                return mid
-        return -1
+                return right+idx
