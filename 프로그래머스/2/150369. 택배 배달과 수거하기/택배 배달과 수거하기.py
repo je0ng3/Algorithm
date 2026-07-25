018be9e3394f52fbf7abafa665f72109aea6f592
Park Jeong-Eun <109335097+je0ng3@users.noreply.github.com>
2026-07-25 15:10:52 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 152.34 ms, Memory: 24.1 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,13 +1,13 @@
 def solution(cap, n, deliveries, pickups):
     answer = 0
-    idps = [(i, d, p) for i, (d, p) in enumerate(zip(deliveries, pickups), 1) if d or p]
-    delivery = pickup = 0
-    while idps:
-        i, d, p = idps.pop() # 멀리있는 집부터
-        delivery += d
+    idps = [(i,d,p) for i, (d, p) in enumerate(zip(deliveries, pickups), 1)]
+    deliver = 0
+    pickup = 0
+    for i, d, p in idps[::-1]:
+        deliver += d
         pickup += p
-        while delivery>0 or pickup>0:
-            delivery -= cap
-            pickup -= cap
+        while deliver>0 or pickup>0:
             answer += i*2
+            deliver -= cap
+            pickup -= cap
     return answer
