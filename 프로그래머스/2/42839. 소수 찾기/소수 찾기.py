fe6d44beda87a0b388499a8950d3d59f9888b295
Park Jeong-Eun <109335097+je0ng3@users.noreply.github.com>
2026-07-25 19:26:36 +0900

    [level 2] Title: 소수 찾기, Time: 4.63 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,17 +1,27 @@
-from itertools import permutations
-
 def solution(numbers):
-    answer = 0
-    cases = [''.join(i)  for j in range(1, len(numbers)+1) for i in permutations(list(numbers), j)]
-    for case in list(set(map(int, cases))):
-        if is_prime(case):
-            answer+=1
-    return answer
-
-def is_prime(num):
-    if num<2:
-        return False
-    for i in range(2, int(num**0.5)+1):
-        if num%i==0:
+    nums = set()
+    visited = [False]*len(numbers)
+    def dfs(cur):
+        if cur:
+            nums.add(int(cur))
+        for i in range(len(numbers)):
+            if not visited[i]:
+                visited[i] = True
+                dfs(cur+numbers[i])
+                visited[i] = False
+    dfs("")
+    
+    def is_prime(n):
+        if n<2:
             return False
-    return True
+        for i in range(2, int(n**0.5)+1):
+            if num%i==0:
+                return False
+        return True
+    
+    answer = 0
+    for num in nums:
+        if is_prime(num):
+            answer += 1
+
+    return answer
