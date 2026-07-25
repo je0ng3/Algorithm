622f27ac527d1a4bd60b2e56ccd56a1cf8e039e6
Park Jeong-Eun <109335097+je0ng3@users.noreply.github.com>
2026-07-25 21:20:39 +0900

    [level 2] Title: 배달, Time: 0.72 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,20 +1,21 @@
+import heapq
 from collections import defaultdict
 
 def solution(N, road, K):
-    answer = 0
-    INF = float('inf')
-    dist = [[INF]*(N+1) for _ in range(N+1)]
-    for i in range(N+1):
-        dist[i][i] = 0
+    graph = defaultdict(list)
     for u, v, w in road:
-        w = min(dist[u][v], w)
-        dist[u][v] = w
-        dist[v][u] = w
-    for k in range(N+1):
-        for i in range(N+1):
-            for j in range(N+1):
-                dist[i][j] = min(dist[i][j], dist[i][k]+dist[k][j])
-    for i in range(1, N+1):
-        if dist[1][i]<=K:
-            answer += 1
+        graph[u].append((v,w))
+        graph[v].append((u, w))
+        
+    heap = [(0,1)] # 시간, 노드
+    dist = [float('inf')]*(N+1)
+    dist[1]=0
+    while heap:
+        time, node = heapq.heappop(heap)
+        for v, w in graph[node]:
+            nxt_time = time+w
+            if nxt_time<=K and nxt_time<dist[v]:
+                dist[v] = nxt_time
+                heapq.heappush(heap, (nxt_time, v))
+    answer = len([d for d in dist if d!=float('inf')])
     return answer
