90c8688ab885cbbc77ee3577bce2e5b13dce9fee
Park Jeong-Eun <109335097+je0ng3@users.noreply.github.com>
2026-07-25 14:54:32 +0900

    [level 2] Title: 양궁대회, Time: 1.47 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.py
@@ -1,53 +1,46 @@
-from collections import deque
-
 def solution(n, info):
-    answer = [-1] 
-        
+    answer = [-1]
+
     # 라이언
-    # 가장 큰 점수 차이, 낮은 점수 많이 맞춘 것 우선
-    lion = [0]*11 # 0~10
+    lion = [0]*11 # 10~0
     max_diff = 0
     
-    def dfs(idx, arrow):
+    def dfs(lion, arrow, point):
         nonlocal answer, max_diff
         
-        if idx==11:
-            # 남은 화살은 0점
-            if arrow>0:
-                lion[10]=arrow
-            # 점수 계산
-            a_score = l_score = 0
-            for i in range(11):
-                score = 10-i
-                if info[i]==0 and lion[i]==0:
-                    continue
-                if lion[i]>info[i]:
-                    l_score += score
-                else:
-                    a_score += score
-            diff = l_score - a_score
-            if max_diff < diff:
-                max_diff = diff
-                answer = lion[:]
-            elif diff>0 and max_diff == diff:
-                if lion[::-1]>answer[::-1]:
+        # 점수 계산
+        if arrow==0 or point==11:
+            lion[10]=arrow # 남은 화살이 있다면 사용
+            a_s = l_s = 0
+            for i, (a, l) in enumerate(zip(info, lion)):
+                if a or l:
+                    s = 10-i
+                    if a<l:
+                        l_s+=s
+                    else:
+                        a_s+=s
+            if a_s<l_s:
+                diff = l_s-a_s
+                if max_diff <diff:
                     answer = lion[:]
-            
-            if arrow>0:
-                lion[10]-=arrow # 백트래킹
+                    max_diff = diff
+                elif max_diff==diff and \
+                    answer[::-1]<lion[::-1]:
+                    answer = lion[:]
+            lion[10]-=arrow # 백트래킹
             return 
         
-        # 과녁 안맞춤
-        dfs(idx+1, arrow)
-        # 과녁 맞춤
-        need = info[idx]+1
-        if arrow>=need:
-            lion[idx] = need
-            dfs(idx+1, arrow-need)
-            lion[idx] = 0 # 백트래킹
-        
-    dfs(0, n)
+        score = 10-point # 점수 인덱스
+        # 현재 점수 포기
+        dfs(lion, arrow, point+1) # 다음 과녁 
+        # 현재 점수 획득
+        if info[score]<arrow: 
+            need = info[score]+1
+            lion[score] = need
+            arrow-= need
+            dfs(lion, arrow, point+1)
+            arrow+= need  # 백트래킹
+            lion[score]-=need
+    
+    dfs(lion, n, 0)
     return answer
-        
-        
-        
