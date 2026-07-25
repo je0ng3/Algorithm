b1d9a7a31b43bd5aa54370289abad2dc6a163e3c
Park Jeong-Eun <109335097+je0ng3@users.noreply.github.com>
2026-07-25 15:45:23 +0900

    [level 3] Title: 순위, Time: 7.58 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -2,30 +2,27 @@ from collections import defaultdict, deque
 
 def solution(n, results):
     answer = 0
-    # win[i]: i를 이긴 선수
-    # lose[i]: i에게 진 선수
-    win = defaultdict(list)
-    lose = defaultdict(list)
+    
+    win = defaultdict(list) # 날 이긴 선수
+    lose = defaultdict(list) # 내가 이긴 선수
     for a, b in results:
         win[b].append(a)
         lose[a].append(b)
+        
+    def bfs(start, graph):
+        visited = set()
+        q = deque([start])
+        while q:
+            cur = q.popleft()
+            for nxt in graph[cur]:
+                if nxt not in visited:
+                    visited.add(nxt)
+                    q.append(nxt)
+        return visited
+        
     for i in range(1, n+1):
-        know = [False]*n
-        know[i-1] = True
-        q = deque(win[i])
-        while q:
-            who = q.popleft()
-            know[who-1] = True
-            for w in win[who]:
-                if not know[w-1]:
-                    q.append(w)
-        q = deque(lose[i])
-        while q:
-            who = q.popleft()
-            know[who-1]=True
-            for l in lose[who]:
-                if not know[l-1]:
-                    q.append(l)
-        if all(know):
-            answer += 1
+        wins = bfs(i, win)
+        loses = bfs(i, lose)
+        if len(wins)+len(loses)+1==n:
+            answer+=1
     return answer
