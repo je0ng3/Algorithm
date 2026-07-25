00bc2ebb3df7c801c726f8581d4bd063491c4e1a
Park Jeong-Eun <109335097+je0ng3@users.noreply.github.com>
2026-07-25 13:21:31 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 196.49 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -1,34 +1,29 @@
 def solution(rows, columns, queries):
     answer = []
-    grid = []
-    for i in range(rows):
-        start = i*columns+1
-        new = [v for v in range(start, start+columns)]
-        grid.append(new)
-    
+    maps = [[i for i in range(columns*j+1, columns*(j+1)+1)] \
+                                    for j in range(rows)]
     for query in queries:
         r1, c1, r2, c2 = [x-1 for x in query]
-        new_grid = [row[:] for row in grid]
-        temp = []
-        for c in range(c1+1, c2+1): # 오른쪽
-            new_grid[r1][c] = grid[r1][c-1]
-            temp.append(grid[r1][c-1])
-        for r in range(r1+1, r2+1): # 아래
-            new_grid[r][c2] = grid[r-1][c2]
-            temp.append(grid[r-1][c2]) # 왼쪽
+        used = []
+        temp = [row[:] for row in maps]
+        for c in range(c1+1, c2+1):
+            maps[r1][c] = temp[r1][c-1]
+            used.append(temp[r1][c-1])
         for c in range(c1, c2):
-            new_grid[r2][c] = grid[r2][c+1]
-            temp.append(grid[r2][c+1])
-        for r in range(r1, r2): # 위
-            new_grid[r][c1] = grid[r+1][c1]
-            temp.append(grid[r+1][c1])
-        grid = new_grid
-        answer.append(min(temp))
-        
+            maps[r2][c] = temp[r2][c+1]
+            used.append(temp[r2][c+1])
+        for r in range(r1, r2):
+            maps[r][c1] = temp[r+1][c1]
+            used.append(temp[r+1][c1])
+        for r in range(r1+1, r2+1):
+            maps[r][c2] = temp[r-1][c2]
+            used.append(temp[r-1][c2])
+        answer.append(min(used))
     return answer
 
-# r1,c1부터 r2,c2까지 회전
-# r, c+1: r1의 c1~c2-1
-# r+1, c: c2의 r1~r2-1
-# r, c-1: r2의 c1+1~c2
-# r-1, c: c1의 r1+1~r2
+
+# r1,c1 ~ r2,c2
+# r1 | c1+1~c2 = c-1
+# r2 | c1~c2-1 = c+1
+# c1 | r1~r2-1 = r+1
+# c2 | r1+1~r2 = r-1
