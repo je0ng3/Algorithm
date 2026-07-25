e9fe43f029525774f5b1e7327e342bf00dc86d27
Park Jeong-Eun <109335097+je0ng3@users.noreply.github.com>
2026-07-25 20:57:52 +0900

    [level 2] Title: 배달, Time: 12.33 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,27 +1,20 @@
 from collections import defaultdict
-import heapq
 
 def solution(N, road, K):
     answer = 0
-    graph = defaultdict(list)
+    INF = float('inf')
+    dist = [[INF]*(N+1) for _ in range(N+1)]
+    for i in range(N+1):
+        dist[i][i] = 0
     for u, v, w in road:
-        graph[u].append((v, w))
-        graph[v].append((u, w))
-    dist = [0]+[float('inf')]*N
-    dist[1] = 0 # 1번에서 시작
-    heap = [(0,1)] # 시간, 마을
-    while heap:
-        time, node = heapq.heappop(heap)
-        for nxt, t in graph[node]:
-            new_time = time+t
-            if new_time<=K and new_time<dist[nxt]:
-                dist[nxt] = new_time
-                heapq.heappush(heap, (new_time, nxt))
-    for d in dist[1:]:
-        if d<=K:
+        w = min(dist[u][v], w)
+        dist[u][v] = w
+        dist[v][u] = w
+    for k in range(N+1):
+        for i in range(N+1):
+            for j in range(N+1):
+                dist[i][j] = min(dist[i][j], dist[i][k]+dist[k][j])
+    for i in range(1, N+1):
+        if dist[1][i]<=K:
             answer += 1
     return answer
-
-# n개의 마을 : 1~n
-# 양방향 통행 도로
-# 1번과 연결된 k 시간 이하의 마을
