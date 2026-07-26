7bc2fe5554f53077fc0a9a89838523a6643dde10
Park Jeong-Eun <109335097+je0ng3@users.noreply.github.com>
2026-07-26 12:17:35 +0900

    [level 3] Title: 합승 택시 요금, Time: 710.97 ms, Memory: 12.5 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,23 +1,17 @@
 def solution(n, s, a, b, fares):
     INF = float('inf')
+    answer = INF
     graph = [[INF]*(n+1) for _ in range(n+1)]
-    for i in range(1, n+1):
+    for i in range(n+1):
         graph[i][i] = 0
-    for c, d, f in fares:
-        graph[c][d] = f
-        graph[d][c] = f
-    
+    for n1, n2, f in fares:
+        graph[n1][n2]=f
+        graph[n2][n1]=f
     for k in range(1, n+1):
         for i in range(1, n+1):
             for j in range(1, n+1):
                 graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
-
-    answer = graph[s][a]+graph[s][b]
-    for i in range(1,n+1):
-        if i==s:
-            continue
-        temp = graph[s][i]
-        temp += graph[i][a] if i!=a else 0
-        temp += graph[i][b] if i!=b else 0
-        answer = min(answer, temp)
+    for i in range(1, n+1):
+        total = graph[s][i]+graph[i][a]+graph[i][b]
+        answer = min(total, answer)
     return answer
