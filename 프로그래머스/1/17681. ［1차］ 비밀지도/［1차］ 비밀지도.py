69c330ba0c40c9cf256e56c7ae6e078082e51e9b
Park Jeong-Eun <109335097+je0ng3@users.noreply.github.com>
2026-07-28 15:10:04 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.09 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,18 +1,20 @@
-def ten2too(i):
-    a = []
-    while i>0:
-        a.append(i%2)
-        i//=2
-    return a
-
 def solution(n, arr1, arr2):
-    answer = ["" for _ in range(n)]
-    one = list(map(ten2too, arr1))
-    two = list(map(ten2too, arr2))
-    for i in range(n):
-        for j in range(n-1,-1,-1):
-            if (len(one[i])>j and one[i][j]==1) or (len(two[i])>j and two[i][j]==1):
-                answer[i]+='#'
+    binary = []
+    for a in arr1:
+        a = bin(a)[2:]
+        a = ['0']*(n-len(a))+list(a)
+        binary.append(a)
+    for i, b in enumerate(arr2):
+        b = list(bin(b)[2:])
+        b = ['0']*(n-len(b))+list(b)
+        binary[i] = list(int(a_) or int(b_) for a_, b_ in zip(binary[i], b))
+    answer = []
+    for row in range(n):
+        temp = ''
+        for col in range(n):
+            if binary[row][col]:
+                temp+='#'
             else:
-                answer[i]+=' '
+                temp+=' '
+        answer.append(temp)
     return answer
