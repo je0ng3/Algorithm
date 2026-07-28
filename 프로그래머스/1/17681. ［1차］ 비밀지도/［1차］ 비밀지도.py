10b8d89415e990c8a2db341031cc2988a1caa812
Park Jeong-Eun <109335097+je0ng3@users.noreply.github.com>
2026-07-28 15:12:41 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,20 +1,10 @@
 def solution(n, arr1, arr2):
-    binary = []
-    for a in arr1:
-        a = bin(a)[2:]
-        a = ['0']*(n-len(a))+list(a)
-        binary.append(a)
-    for i, b in enumerate(arr2):
-        b = list(bin(b)[2:])
-        b = ['0']*(n-len(b))+list(b)
-        binary[i] = list(int(a_) or int(b_) for a_, b_ in zip(binary[i], b))
-    answer = []
-    for row in range(n):
-        temp = ''
-        for col in range(n):
-            if binary[row][col]:
-                temp+='#'
-            else:
-                temp+=' '
-        answer.append(temp)
-    return answer
+    maps = []
+    for i in range(n):
+        maps.append(
+            bin(arr1[i]|arr2[i])[2:]
+                .zfill(n)
+                .replace('1', '#')
+                .replace('0', ' ')
+        )
+    return maps
