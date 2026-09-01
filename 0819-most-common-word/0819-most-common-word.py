9f263a758497a1b2e5d45080cb920cd4423aa2da
Jungeun Park <jepark2934@gmail.com>
2026-09-01 15:40:04 +0900

    Time: 9 ms (8.06%), Space: 19.2 MB (85.04%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.py
+++ 0819-most-common-word/0819-most-common-word.py
@@ -3,8 +3,8 @@ from collections import Counter
 
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        banned = set(banned)
-        filter_p = re.sub(r'\W', ' ', paragraph.lower())
-        words = [word for word in filter_p.split() if word not in banned]
+        
+        words = [word for word in re.sub(r'\W', ' ', paragraph)
+                    .lower().split() if word not in set(banned)]
         counts = Counter(words)
         return counts.most_common(1)[0][0]
