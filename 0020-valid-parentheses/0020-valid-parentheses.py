ee062f9d9a72828f35462bf36c898d1ef2b5b526
Jungeun Park <jepark2934@gmail.com>
2026-09-04 13:56:16 +0900

    Time: 3 ms (32.01%), Space: 19.4 MB (23.9%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,14 +1,20 @@
+from collections import deque
+
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = collections.deque()
-        brackets = {'(':')', '{':'}', '[':']'}
+        if len(s)%2==1:
+            return False
+
+        dic = {
+            '(':')', 
+            '{':'}',
+            '[':']'
+        }
+        stack = deque()
         for c in s:
-            if c in brackets:
+            if c in dic:
                 stack.append(c)
             else:
-                if len(stack)==0:
+                if not stack or dic[stack.pop()]!=c:
                     return False
-                check = stack.pop()
-                if brackets[check]!=c:
-                    return False
-        return True if len(stack)==0 else False
+        return len(stack)==0
