72f0a7b2b3bb2b8af4c6c1b71f77e126da574ddb
Jungeun Park <jepark2934@gmail.com>
2026-09-04 15:30:02 +0900

    Time: 5 ms (61.81%), Space: 22.9 MB (60.56%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,10 +1,6 @@
+from collections import Counter
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counts = collections.Counter(nums)
-        heap = []
-        for count in counts:
-            heapq.heappush(heap, (-counts[count], count))
-        answer = []
-        for _ in range(k):
-            answer.append(heapq.heappop(heap)[1])
-        return answer
+        counts = Counter(nums)
+        return [k for k, v in counts.most_common(k)]
