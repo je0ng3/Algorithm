95ecae7a2054bd8bcbc791d9f6202893b4897302
Jungeun Park <jepark2934@gmail.com>
2026-09-04 14:27:23 +0900

    Time: 3 ms (64.68%), Space: 19.5 MB (16.3%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -1,17 +1,14 @@
+from collections import Counter, deque
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        counters = collections.Counter(s)
-        seen = set()
-        stack = collections.deque()
-
+        counts, seen, stack = Counter(s), set(), deque()
         for c in s:
-            counters[c]-=1
+            counts[c]-=1
             if c in seen:
                 continue
-
-            while stack and c < stack[-1] and counters[stack[-1]]>0:
+            # c가 stack에 담긴 것들보다 앞에 오는게 사전순으로 빠른 경우
+            while stack and c<stack[-1] and counts[stack[-1]]>0:
                 seen.remove(stack.pop())
             stack.append(c)
             seen.add(c)
-
         return ''.join(stack)
