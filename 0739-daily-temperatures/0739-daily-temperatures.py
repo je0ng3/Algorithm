9ee816d55bdd47abdb2310104474d5f2cdbc7b19
Jungeun Park <jepark2934@gmail.com>
2026-09-04 14:33:38 +0900

    Time: 97 ms (50.38%), Space: 28.3 MB (62.11%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,10 +1,12 @@
+from collections import deque
+
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        answer = [0 for _ in range(len(temperatures))]
-        stack = collections.deque()
+        answer = [0]*len(temperatures)
+        q = deque()
         for i, t in enumerate(temperatures):
-            while stack and temperatures[stack[-1]]<t:
-                a = stack.pop()
-                answer[a] = i-a
-            stack.append(i)
+            while q and temperatures[q[-1]]<t:
+                idx = q.pop()
+                answer[idx] = i-idx
+            q.append(i)
         return answer
