df080b96864d8a6230a48cf4d9750f565bed5499
Jungeun Park <jepark2934@gmail.com>
2026-09-04 15:15:15 +0900

    Time: 4 ms (92.38%), Space: 23.2 MB (16.45%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,22 +3,21 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def mergeTwo(self, l1, l2):
-        if l1 and l2:
-            if l1.val>l2.val:
-                l1, l2 = l2, l1
-            l1.next = self.mergeTwo(l1.next, l2)
-        return l1 or l2
+import heapq
 
+class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists:
-            return None
-        while len(lists)>1:
-            merged = []
-            for i in range(0, len(lists), 2):
-                l1 = lists[i]
-                l2 = lists[i+1] if i+1<len(lists) else None
-                merged.append(self.mergeTwo(l1, l2))
-            lists = merged
-        return lists[0]
+        root = result = ListNode(None)
+        heap = []
+        for i, l in enumerate(lists):
+            if l:
+                heapq.heappush(heap, (l.val, i, l))
+        while heap:
+            node = heapq.heappop(heap)
+            idx = node[1]
+            result.next = node[2]
+            
+            result = result.next
+            if result.next:
+                heapq.heappush(heap, (result.next.val, idx, result.next))
+        return root.next
