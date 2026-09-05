00beb802d826b73835a0f7b81dddacd562d88a8d
Jungeun Park <jepark2934@gmail.com>
2026-09-05 17:19:27 +0900

    Time: 86 ms (53.03%), Space: 19.6 MB (86.2%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -3,12 +3,13 @@ from collections import Counter
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         # 윈도우 내 출현 빈도가 가장 높은 문자의 수를 뺀 값이 k와 같을 수 있는 수 중 가장 큰 최댓값
-        left = right = 0
+        left = 0
         counts = Counter()
-        for right in range(1, len(s)+1):
-            counts[s[right-1]]+=1
-            max_char_n = counts.most_common(1)[0][1]
-            if right-left-max_char_n>k:
+        max_char_n = 0
+        for right, c in enumerate(s):
+            counts[c] += 1
+            max_char_n = max(max_char_n, counts[c])
+            if right-left+1 -max_char_n > k:
                 counts[s[left]]-=1
                 left +=1
-        return right-left
+        return right-left+1
