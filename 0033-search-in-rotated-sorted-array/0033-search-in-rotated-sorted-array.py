35011e4653b262ade92582bc0363650cf2e6a3c3
Jungeun Park <jepark2934@gmail.com>
2026-09-05 16:39:03 +0900

    Time: 0 ms (100%), Space: 19.7 MB (10.5%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,28 +1,10 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        def binary_search(lst, t, l, r):
-            while l<=r:
-                mid = (l+r)//2
-                if lst[mid]<t:
-                    l=mid+1
-                elif lst[mid]>t:
-                    r=mid-1
-                else:
-                    return mid
-            return -1
-
-        if not nums or target not in set(nums):
-            return -1
-        answer = 0
-        # 회전 지점 탐색
-        left = right = 0
-        while right<len(nums) and nums[left]<=nums[right]:
-            right+=1
-        # target이 속한 리스트 찾기
-        # 1. left~right-1
-        # 2. right~len(nums)-1
-        if nums[left]<=target<=nums[right-1]:
-            idx = binary_search(nums, target, left, right-1)
-        else:
-            idx = binary_search(nums, target, right, len(nums)-1)
-        return idx
+        pivot = nums.index(min(nums))
+        left = nums[:pivot]
+        right = nums[pivot:]
+        if target in set(left):
+            return left.index(target)
+        if target in set(right):
+            return pivot+right.index(target)
+        return -1
