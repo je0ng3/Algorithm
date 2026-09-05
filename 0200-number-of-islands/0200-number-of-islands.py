9453939a95e4e37679f2583111c3fbb14da9d8bc
Jungeun Park <jepark2934@gmail.com>
2026-09-05 15:39:29 +0900

    Time: 253 ms (36.99%), Space: 21.6 MB (73.1%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,22 +1,24 @@
+from collections import deque
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
-
-        def dfs(grid, r, c):
-            grid[r][c] = '0'
-            dr = [0,0,1,-1]
-            dc = [1,-1,0,0]
-            for i in range(4):
-                nr, nc = r+dr[i], c+dc[i]
-                if 0<=nr<m and 0<=nc<n and grid[nr][nc]=='1':
-                    dfs(grid, nr, nc)
-            
-        count = 0
-        for r in range(m):
-            for c in range(n):
-                if grid[r][c] == '1':
-                    dfs(grid, r, c)
-                    count += 1
+        def dfs(row, col):
+            grid[row][col] = "0"
+            stack = deque([(row, col)])
+            while stack:
+                cr, cc = stack.pop()
+                for dr, dc in [(0,1), (1,0), (0,-1),(-1,0)]:
+                    nr, nc = cr+dr, cc+dc
+                    if 0<=nr<len(grid) and 0<=nc<len(grid[0]) and grid[nr][nc]=="1":
+                        grid[nr][nc]="0"
+                        stack.append((nr,nc))
+            return 
         
-        return count
+        lslands = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c]=="1":
+                    dfs(r, c)
+                    lslands += 1
+        
+        return lslands
