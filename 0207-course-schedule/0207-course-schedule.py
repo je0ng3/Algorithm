0410f9909c2ba7c53e2626c9f71a978dd58fd827
Jungeun Park <jepark2934@gmail.com>
2026-09-05 23:52:50 +0900

    Time: 6 ms (48.72%), Space: 21.5 MB (11.43%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,24 +1,27 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # 선수과목 그래프에 사이클이 존재하면 False
         graph = collections.defaultdict(list)
         for a, b in prerequisites:
             graph[a].append(b)
+        
+        visited = set()
+        visiting = set()
 
-        done = set()
-        def dfs(i, check):
-            if i not in graph or i in done:
-                done.add(i)
-                return True
-            if i in check:
-                return False
-            check.add(i)
-            for pre in graph[i]:
-                if not dfs(pre, check):
+        def dfs(cur):
+            if cur in visiting:
+                return False # 사이클
+            if cur in visited:
+                return True # 사이클 없이 완료 가능함을 이미 확인
+            visiting.add(cur)
+            for nxt in graph[cur]:
+                if not dfs(nxt):
                     return False
-            done.add(i)
+            visiting.remove(cur)
+            visited.add(cur)
             return True
-            
+        
         for i in range(numCourses):
-            if not dfs(i, set()):
-                return(False)
-        return(True)
+            if not dfs(i):
+                return False
+        return True
